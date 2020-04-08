6106721e163a650a47355e47180501e74312cd93
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-04-08 15:03:55 -0400

    added music

--- src/js/song-playing.js
+++ src/js/song-playing.js
@@ -1,1 +1,37 @@
-alert('song-plying.js')
+
+//FOR PLAYING THE SONG
+let song = document.createElement('audio');
+songChoice();
+
+//Returns Chosen Song File
+function songChoice(){
+  //TODO add parameter to change song
+  return song.setAttribute('src', '/audio_files/ease_your_social.mp3');
+}
+
+//Play Song
+function playSong() {
+  song.play();
+}
+
+//Pause Song
+function pauseSong() {
+  song.pause();
+}
+
+
+//FOR MICROPHONE CONTROL
+let mic;
+function setup()
+{
+  createCanvas(200, 200);
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw() {
+  background(0);
+  let volume = mic.getLevel();
+  
+  ellipse(100, 100, volume * 200, volume * 200);
+}
