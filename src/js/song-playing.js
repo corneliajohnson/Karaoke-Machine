24c6443ac335173ffc46caf39f38c48e7965d8c4
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-04-08 19:16:49 -0400

    added visual audio animation

--- src/js/song-playing.js
+++ src/js/song-playing.js
@@ -24,40 +24,62 @@ function pauseSong() {
 
 
 //MICROPHONE CONTROL
-let mic;
-let volumeHistory = [];
+console.clear();
 
-function setup()
-{
-  let canvas = document.querySelector('.canvas');
-  //createCanvas(400, 400);
-  mic = new p5.AudioIn();
-  mic.start();
+const audioCtx = new AudioContext();
+const numberOfNodes = 256;
+const data = new Uint8Array(numberOfNodes * 4);
+
+const elVisualizer = document.querySelector('.canvas');
+
+const elNodes = Array.from({ length: numberOfNodes }, (n,i)=> {
+  let node = document.createElement('div');
+  node.className = 'node';
+  node.style.setProperty('--i',i);
+  elVisualizer.appendChild(node);
+  return node;
+});
+
+const analyserNode = new AnalyserNode(audioCtx, {
+  fftSize: Math.max(numberOfNodes * 4, 32),
+  maxDecibels: -20,
+  minDecibels: -80,
+  smoothingTimeConstant: 0.8
+});
+
+function updateVisualizer(){
+  requestAnimationFrame(updateVisualizer);
+  
+  analyserNode.getByteFrequencyData(data);
+
+  elNodes.forEach( (node,i) => {
+    node.style.setProperty('--c', data[i]);
+    node.style.setProperty(
+      '--level', 
+      (data[i] / 255) 
+      // Attempt a log-ish scale for sensitivity in higher registers
+      * (1 + (i / numberOfNodes))
+    );
+  });
+  
+  //window.volume.textContent = Math.round( (data[0] / 255) * 10) / 10;
+  
 }
 
-function draw() {
-  //background(0);
-  let volume = mic.getLevel();
-  volumeHistory.push(volume);
-
-  stroke(255);
-  noFill();
-
-  let currentY = map(volume, 0, 1, height, 0);
-
-  translate(0, -height / 2 + currentY);
-  beginShape();
-
-  for (let i = 0; i < volumeHistory.length; i++) {
-    let y = map(volumeHistory[i], 0, 1, height, 0);
-    vertex(i, y);
-  }
-
-  endShape();
-
-  if (volumeHistory.length > width) {
-    volumeHistory.splice(0, 1);
-  }
-
-  //console.log(volume);
+function startStream(){
+  window.volume.innerHTML = ' &nbsp; ';
+  
+  return navigator.mediaDevices
+    .getUserMedia({ audio: true, video: false })
+  .then( stream => audioCtx.createMediaStreamSource(stream) )
+  .then( source => {
+    source.connect(analyserNode);
+  }).then(updateVisualizer);
+        
 }
+
+// Have to initialize via a user event
+document.addEventListener('click', ()=>{
+  audioCtx.resume();
+  startStream();
+});
